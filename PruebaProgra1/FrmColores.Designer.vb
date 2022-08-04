59917485efd6496fc67a6984a5aca88616c44716
danie <danie@DESKTOP-QP13Q03>
2022-08-04 08:31:09 -0600

    Agregar archivos de proyecto.

---
+++ PruebaProgra1/FrmColores.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmColores
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtColores = New System.Windows.Forms.TextBox()
+        Me.btnRojo = New System.Windows.Forms.Button()
+        Me.btnVerde = New System.Windows.Forms.Button()
+        Me.btnAmarillo = New System.Windows.Forms.Button()
+        Me.btnMorado = New System.Windows.Forms.Button()
+        Me.btnAzul = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtColores
+        '
+        Me.txtColores.Location = New System.Drawing.Point(287, 58)
+        Me.txtColores.Multiline = True
+        Me.txtColores.Name = "txtColores"
+        Me.txtColores.Size = New System.Drawing.Size(585, 326)
+        Me.txtColores.TabIndex = 0
+        '
+        'btnRojo
+        '
+        Me.btnRojo.Location = New System.Drawing.Point(78, 58)
+        Me.btnRojo.Name = "btnRojo"
+        Me.btnRojo.Size = New System.Drawing.Size(168, 46)
+        Me.btnRojo.TabIndex = 1
+        Me.btnRojo.Text = "Rojo"
+        Me.btnRojo.UseVisualStyleBackColor = True
+        '
+        'btnVerde
+        '
+        Me.btnVerde.Location = New System.Drawing.Point(78, 128)
+        Me.btnVerde.Name = "btnVerde"
+        Me.btnVerde.Size = New System.Drawing.Size(168, 46)
+        Me.btnVerde.TabIndex = 2
+        Me.btnVerde.Text = "Verde"
+        Me.btnVerde.UseVisualStyleBackColor = True
+        '
+        'btnAmarillo
+        '
+        Me.btnAmarillo.Location = New System.Drawing.Point(78, 198)
+        Me.btnAmarillo.Name = "btnAmarillo"
+        Me.btnAmarillo.Size = New System.Drawing.Size(168, 46)
+        Me.btnAmarillo.TabIndex = 3
+        Me.btnAmarillo.Text = "Amarillo"
+        Me.btnAmarillo.UseVisualStyleBackColor = True
+        '
+        'btnMorado
+        '
+        Me.btnMorado.Location = New System.Drawing.Point(78, 338)
+        Me.btnMorado.Name = "btnMorado"
+        Me.btnMorado.Size = New System.Drawing.Size(168, 46)
+        Me.btnMorado.TabIndex = 4
+        Me.btnMorado.Text = "Morado"
+        Me.btnMorado.UseVisualStyleBackColor = True
+        '
+        'btnAzul
+        '
+        Me.btnAzul.Location = New System.Drawing.Point(78, 268)
+        Me.btnAzul.Name = "btnAzul"
+        Me.btnAzul.Size = New System.Drawing.Size(168, 46)
+        Me.btnAzul.TabIndex = 5
+        Me.btnAzul.Text = "Azul"
+        Me.btnAzul.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Coral
+        Me.ClientSize = New System.Drawing.Size(898, 450)
+        Me.Controls.Add(Me.btnAzul)
+        Me.Controls.Add(Me.btnMorado)
+        Me.Controls.Add(Me.btnAmarillo)
+        Me.Controls.Add(Me.btnVerde)
+        Me.Controls.Add(Me.btnRojo)
+        Me.Controls.Add(Me.txtColores)
+        Me.Name = "Form1"
+        Me.Text = "Colores"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtColores As TextBox
+    Friend WithEvents btnRojo As Button
+    Friend WithEvents btnVerde As Button
+    Friend WithEvents btnAmarillo As Button
+    Friend WithEvents btnMorado As Button
+    Friend WithEvents btnAzul As Button
+End Class
---
+++ PruebaProgra1/FrmColores.vb
@@ -0,0 +1,21 @@
+﻿Public Class FrmColores
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnAzul.Click
+        txtColores.BackColor = Color.Blue
+    End Sub
+
+    Private Sub btnRojo_Click(sender As Object, e As EventArgs) Handles btnRojo.Click
+        txtColores.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnVerde_Click(sender As Object, e As EventArgs) Handles btnVerde.Click
+        txtColores.BackColor = Color.Green
+    End Sub
+
+    Private Sub btnAmarillo_Click(sender As Object, e As EventArgs) Handles btnAmarillo.Click
+        txtColores.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub btnMorado_Click(sender As Object, e As EventArgs) Handles btnMorado.Click
+        txtColores.BackColor = Color.DarkViolet
+    End Sub
+End Class
